334e87559ca3c2f14f707c7ab670c50339104713
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2022-02-06 23:18:51 -0600

    refactor: use non-conflicting name for Object type

--- types/auth.d.ts
+++ types/auth.d.ts
@@ -2,12 +2,12 @@ import {GetServerSideProps, GetServerSidePropsContext, Redirect} from 'next';
 import {Session} from 'next-auth';
 import {PreviewData} from 'next/types';
 import {ParsedUrlQuery} from 'querystring';
-import {Object} from './object';
+import {AnyObject} from './object';
 
 type ShouldRedirect = (session: Session) => boolean;
 
 type GetRedirect = (context: GetServerSidePropsContext) => Redirect
 
 export type Auth = (checkSession: ShouldRedirect, getRedirect: GetRedirect, auth: boolean) =>
-    <P extends Object = Object, Q extends ParsedUrlQuery = ParsedUrlQuery, D extends PreviewData = PreviewData>(getServerSideProps?: GetServerSideProps<P, Q, D>) =>
+    <P extends AnyObject = AnyObject, Q extends ParsedUrlQuery = ParsedUrlQuery, D extends PreviewData = PreviewData>(getServerSideProps?: GetServerSideProps<P, Q, D>) =>
         GetServerSideProps<P, Q, D>
