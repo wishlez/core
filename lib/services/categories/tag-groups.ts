53b090a2e90156af56d4fd035696fc68c15eb321
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2022-02-19 13:27:48 -0600

    chore: remove unused functions

--- lib/services/categories/tag-groups.ts
+++ lib/services/categories/tag-groups.ts
@@ -1,23 +1,6 @@
-import {Prisma} from '@prisma/client';
 import {getPrismaClient} from '../../prisma';
 
 const prisma = getPrismaClient();
-
-export const getTagGroups = async (groupId: number) => await prisma.categoryTagGroup.findMany({
-    where: {
-        groupId
-    }
-});
-
-export const createTagGroup = async (data: Prisma.CategoryTagGroupUncheckedCreateInput) => await prisma.categoryTagGroup.create({
-    data
-});
-
-export const deleteTagGroup = async (id: number) => await prisma.categoryTagGroup.delete({
-    where: {
-        id
-    }
-});
 
 export const updateTagGroup = async (groupId: number, added: number[], deleted: number[]) => prisma.$transaction([
     prisma.categoryTagGroup.deleteMany({
