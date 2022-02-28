236c4149a1d1a9a6404ef3cea68cb59bd8df6049
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2022-02-27 20:05:03 -0600

    feat: create formatted amount

---
+++ lib/design/formatted-amount.tsx
@@ -0,0 +1,22 @@
+import {FunctionComponent} from 'react';
+import styled from 'styled-components';
+
+type Props = {
+    number: string | number
+}
+
+const format = new Intl.NumberFormat('en', {
+    maximumFractionDigits: 2,
+    minimumFractionDigits: 2
+});
+
+export const Amount = styled.span`
+    font-family: 'CutiveMono', sans-serif;
+    text-align: right;
+`;
+
+export const FormattedAmount: FunctionComponent<Props> = (props) => (
+    <>
+        {format.format(Number(props.number))}
+    </>
+);
