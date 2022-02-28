9ccb935233edbeccec001584e1ada17357be6bec
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2022-02-27 20:05:18 -0600

    feat: add more icon sizes

--- lib/design/icon.tsx
+++ lib/design/icon.tsx
@@ -1,19 +1,25 @@
 import styled from 'styled-components';
 
 type Props = {
-    type: 'add' | 'create' | 'delete'
+    type: 'add' | 'create' | 'delete' | 'east'
+    size?: 'normal' | 'in-text'
 }
 
 export const Icon = styled.i<Props>`
     font-family: 'Material Icons', serif;
-    font-size: 1.5em;
+    font-size: ${(props) => props.size === 'in-text' ? '1' : '1.5'}em;
     font-style: normal;
     font-variant: none;
     text-transform: none;
     height: 1em;
     width: 1em;
+    vertical-align: -.125em;
 
     &:after {
         content: "${(props) => props.type}";
     }
 `;
+
+Icon.defaultProps = {
+    size: 'normal'
+};
