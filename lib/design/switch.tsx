4c783e19a645c9e80e4f97e419d1650d88b0c167
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2022-04-28 08:14:51 -0500

    fix: align switch control to start

--- lib/design/switch.tsx
+++ lib/design/switch.tsx
@@ -14,6 +14,7 @@ const Container = styled.div`
     display: grid;
     grid-auto-flow: column;
     grid-gap: var(--grid-gap);
+    justify-content: start;
 `;
 
 const _Switch = styled.label<{ isOn: boolean }>`
